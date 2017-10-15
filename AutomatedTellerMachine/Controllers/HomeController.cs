c4afa42d41d26b92b7d3c28ef0ebde4675415afc
cynthia.quach <ccquach93@gmail.com>
2017-10-15 15:02:59 -0700

    Apply MyLoggingFilter to Index action

--- AutomatedTellerMachine/Controllers/HomeController.cs
+++ AutomatedTellerMachine/Controllers/HomeController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AutomatedTellerMachine.Filters;
 
 namespace AutomatedTellerMachine.Controllers
 {
     public class HomeController : Controller
     {
         // GET: /home/index/
+        [MyLoggingFilter]
         public ActionResult Index()
         {
             return View();
