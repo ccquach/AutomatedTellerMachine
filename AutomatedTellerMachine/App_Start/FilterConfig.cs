6fcf5a4f08790f4ee32e632847e73771b9ac5724
cynthia.quach <ccquach93@gmail.com>
2017-10-15 15:04:14 -0700

    Apply MyLoggingFilter attribute globally so that it applies to all actions

--- AutomatedTellerMachine/App_Start/FilterConfig.cs
+++ AutomatedTellerMachine/App_Start/FilterConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web;
 using System.Web.Mvc;
+using AutomatedTellerMachine.Filters;
 
 namespace AutomatedTellerMachine
 {
@@ -8,6 +9,7 @@ namespace AutomatedTellerMachine
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new MyLoggingFilterAttribute());
         }
     }
 }
