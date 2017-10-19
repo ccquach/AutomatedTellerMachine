0adb871f8cec457c51ba8fab9f1398f2db286f1a
cynthia.quach <ccquach93@gmail.com>
2017-10-18 21:32:58 -0700

    Add Display attribute to checking account properties

--- AutomatedTellerMachine/Models/CheckingAccount.cs
+++ AutomatedTellerMachine/Models/CheckingAccount.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,17 @@ namespace AutomatedTellerMachine.Models
     public class CheckingAccount
     {
         public int Id { get; set; }
+
+        [Display(Name = "Account #")]
         public string AccountNumber { get; set; }
+
+        [Display(Name = "First name")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last name")]
         public string LastName { get; set; }
+
+
         public decimal Balance { get; set; }
     }
 }
