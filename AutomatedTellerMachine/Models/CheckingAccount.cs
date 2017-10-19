d235cf2099d35052ae5c8dd4d6d59907fa399a90
cynthia.quach <ccquach93@gmail.com>
2017-10-18 20:52:24 -0700

    Add properties to checking account model

--- AutomatedTellerMachine/Models/CheckingAccount.cs
+++ AutomatedTellerMachine/Models/CheckingAccount.cs
@@ -7,5 +7,10 @@ namespace AutomatedTellerMachine.Models
 {
     public class CheckingAccount
     {
+        public int Id { get; set; }
+        public string AccountNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Balance { get; set; }
     }
 }
