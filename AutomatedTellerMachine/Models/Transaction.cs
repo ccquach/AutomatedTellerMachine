ae6e80d8cf1870cd57d50d06773123e5cfe3dcca
cynthia.quach <ccquach93@gmail.com>
2017-10-22 12:45:11 -0700

    Create Transaction model for deposits and withdrawals

---
+++ AutomatedTellerMachine/Models/Transaction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutomatedTellerMachine.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public int CheckingAccountId { get; set; }
+    }
+}
