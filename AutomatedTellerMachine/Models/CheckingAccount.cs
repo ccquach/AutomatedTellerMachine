f174792e10f26274b6993653d35530cd1b943336
cynthia.quach <ccquach93@gmail.com>
2017-10-22 11:38:01 -0700

    Add StringLength and Column attributes to Account property as data type constraints

--- AutomatedTellerMachine/Models/CheckingAccount.cs
+++ AutomatedTellerMachine/Models/CheckingAccount.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -11,7 +12,8 @@ namespace AutomatedTellerMachine.Models
         public int Id { get; set; }
 
         [Required]
-        //[StringLength(maximumLength:10, MinimumLength = 6)]
+        [StringLength(maximumLength: 10)]
+        [Column(TypeName = "varchar")]
         [RegularExpression(@"\d{6,10}", ErrorMessage = "Account # must be between 6 and 10 digits.")]
         [Display(Name = "Account #")]
         public string AccountNumber { get; set; }
