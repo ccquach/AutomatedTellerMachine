90cd754a1a09c49fa3285db6be36ba3ce8e45f9d
cynthia.quach <ccquach93@gmail.com>
2017-10-22 21:44:30 -0700

    Remove unnecessary actions (could have created empty controller since only implementing Deposit action)

--- AutomatedTellerMachine/Controllers/TransactionController.cs
+++ AutomatedTellerMachine/Controllers/TransactionController.cs
@@ -9,18 +9,6 @@ namespace AutomatedTellerMachine.Controllers
 {
     public class TransactionController : Controller
     {
-        // GET: Transaction
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        // GET: Transaction/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
         // GET: Transaction/Deposit
         public ActionResult Deposit()
         {
@@ -38,50 +26,6 @@ namespace AutomatedTellerMachine.Controllers
             try
             {
                 // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Transaction/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Transaction/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Transaction/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Transaction/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
 
                 return RedirectToAction("Index");
             }
