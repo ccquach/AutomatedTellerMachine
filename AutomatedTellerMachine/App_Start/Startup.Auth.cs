5cdb200fa5ba309a441298327fa8950ea96b4e7a
cynthia.quach <ccquach93@gmail.com>
2017-10-24 20:19:29 -0700

    Enable Facebook authorization

--- AutomatedTellerMachine/App_Start/Startup.Auth.cs
+++ AutomatedTellerMachine/App_Start/Startup.Auth.cs
@@ -54,9 +54,9 @@ namespace AutomatedTellerMachine
             //   consumerKey: "",
             //   consumerSecret: "");
 
-            //app.UseFacebookAuthentication(
-            //   appId: "",
-            //   appSecret: "");
+            app.UseFacebookAuthentication(
+               appId: "",
+               appSecret: "");
 
             //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             //{
