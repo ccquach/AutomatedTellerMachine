3c06742fe952b4c6b9694eda041874ae98cf75a1
cynthia.quach <ccquach93@gmail.com>
2017-10-29 16:05:32 -0700

    Implement fake db context

--- AutomatedTellerMachine/Models/ApplicationDbContext.cs
+++ AutomatedTellerMachine/Models/ApplicationDbContext.cs
@@ -30,4 +30,15 @@ namespace AutomatedTellerMachine.Models
         public IDbSet<CheckingAccount> CheckingAccounts { get; set; }
         public IDbSet<Transaction> Transactions { get; set; }
     }
+
+    public class FakeApplicationDbContext : IApplicationDbContext
+    {
+        public IDbSet<CheckingAccount> CheckingAccounts { get; set; }
+        public IDbSet<Transaction> Transactions { get; set; }
+
+        public int SaveChanges()
+        {
+            return 0;
+        }
+    }
 }
