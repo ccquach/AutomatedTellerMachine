e4dca5f4cc5791e5b1a0c7c3fb8b4e9e316e265a
cynthia.quach <ccquach93@gmail.com>
2017-10-29 15:46:38 -0700

    Move ApplicationDbContext class to its own model class file, since handling more than identities

---
+++ AutomatedTellerMachine/Models/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AutomatedTellerMachine.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public DbSet<CheckingAccount> CheckingAccounts { get; set; }
+
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
