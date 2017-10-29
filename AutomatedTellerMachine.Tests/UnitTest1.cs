b9dff85241d7dd751abdbc5a89e8e6783c07db70
cynthia.quach <ccquach93@gmail.com>
2017-10-29 14:01:45 -0700

    Rename method with meaningful name stating obvious purpose

--- AutomatedTellerMachine.Tests/UnitTest1.cs
+++ AutomatedTellerMachine.Tests/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace AutomatedTellerMachine.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FooActionReturnsAboutView()
         {
         }
     }
