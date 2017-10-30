ebad9da9f03169153ce53b5477731866f47c3fb6
cynthia.quach <ccquach93@gmail.com>
2017-10-29 17:29:31 -0700

    Remove hard-coded balance from unit test to check if refactored code results in expected balance

--- AutomatedTellerMachine.Tests/UnitTest1.cs
+++ AutomatedTellerMachine.Tests/UnitTest1.cs
@@ -43,7 +43,6 @@ namespace AutomatedTellerMachine.Tests
 
             // ACT
             transactionController.Deposit(new Transaction { CheckingAccountId = 1, Amount = 25 });
-            checkingAccount.Balance = 25;
 
             // ASSERT
             Assert.AreEqual(25, checkingAccount.Balance);
