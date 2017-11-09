93b1128478b9ce9407a5f7a57f22aa5a93dcfee4
cynthia.quach <ccquach93@gmail.com>
2017-11-08 21:06:46 -0800

    Added SaveChanges function to enumerate errors within EntityValidationErrors collection (stackoverflow)

--- AutomatedTellerMachine/Migrations/Configuration.cs
+++ AutomatedTellerMachine/Migrations/Configuration.cs
@@ -7,7 +7,9 @@ namespace AutomatedTellerMachine.Migrations
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
+    using System.Data.Entity.Validation;
     using System.Linq;
+    using System.Text;
 
     internal sealed class Configuration : DbMigrationsConfiguration<AutomatedTellerMachine.Models.ApplicationDbContext>
     {
@@ -68,6 +70,33 @@ namespace AutomatedTellerMachine.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+        }
+
+        private void SaveChanges(DbContext context)
+        {
+            try
+            {
+                context.SaveChanges();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var failure in ex.EntityValidationErrors)
+                {
+                    sb.AppendFormat("{0} failed validation\n", failure.Entry.Entity.GetType());
+                    foreach (var error in failure.ValidationErrors)
+                    {
+                        sb.AppendFormat("- {0} : {1}", error.PropertyName, error.ErrorMessage);
+                        sb.AppendLine();
+                    }
+                }
+
+                throw new DbEntityValidationException(
+                    "Entity Validation Failed - errors follow:\n" +
+                    sb.ToString(), ex
+                ); // Add the original exception as the innerException
+            }
         }
     }
 }
